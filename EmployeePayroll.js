0a4df04b63cffc5701cd0f0d7e8f85519572d632
mohan <sahumohan338@gmail.com>
2022-09-09 18:46:24 +0530

    [mohan]add: check employee present or not

---
+++ EmployeePayroll.js
@@ -0,0 +1,9 @@
+const Is_present=1;
+let empCheck=Math.floor(Math.random()*10)%2;
+
+if(empCheck==Is_present)
+{
+    console.log("employee is present");
+}else{
+    console.log("employee is absent");
+}
