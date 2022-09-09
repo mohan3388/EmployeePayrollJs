81eb306f9f7e81b404c686ca3bd7b328ea30b86e
mohan <sahumohan338@gmail.com>
2022-09-09 18:50:54 +0530

    [mohan]add: Calculate Employee Wage

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,3 +1,4 @@
+//Uc1 check employee
 const Is_present=1;
 let empCheck=Math.floor(Math.random()*10)%2;
 
@@ -6,4 +7,27 @@ if(empCheck==Is_present)
     console.log("employee is present");
 }else{
     console.log("employee is absent");
+}
+
+
+//Uc2 switch case
+const Is_Fulltime=2;
+const Is_Parttime=1;
+const Parttime_Hours=4;
+const Fulltime_Hours=8;
+const Wage_Per_Hours=20;
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random()*10)%2;
+    switch(empCheck){
+        case Is_Parttime:
+            empHrs=Parttime_Hours;
+            break;
+        case Is_Fulltime:
+           empHrs=Fulltime_Hours;
+        default:
+            empHrs=0;
+    }
+    let empWage = empHrs*Wage_Per_Hours;
+    console.log("calculate empwage: "+empWage);
 }
